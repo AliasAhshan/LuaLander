b884d628c22ffc55571ee6380c45ad2b224c50fb
AliasAhshan <ahshan.ahmed003@gmail.com>
2025-09-16 15:57:32 -0400

    Created terrain and dotvector setup for lander

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -24,6 +24,23 @@ public class Lander : MonoBehaviour {
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision2D) {
+        float softLandingVelocityMagnitude = 4f;
+        if (collision2D.relativeVelocity.magnitude > softLandingVelocityMagnitude) {
+            Debug.Log("Landed too hard!");
+            return;
+        }
+
+        float dotVector = Vector2.Dot(Vector2.up, transform.up);
+        float minDotVector = .9f;
+        if (dotVector < minDotVector) {
+            Debug.Log("Landed on a too steep angle!");
+            return;
+        }
+
+        Debug.Log("Successful Landing");
+    }
+
     private void Update() {
 
     }
