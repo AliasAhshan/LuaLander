b80fccdf2bde1827e68da1c252d0d9aa53a72537
AliasAhshan <ahshan.ahmed003@gmail.com>
2025-09-17 16:40:11 -0400

    Created game manager script to handle UI and score related features so far and created stats UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
 
+    public static GameManager Instance { get; private set; }
 
     private int score;
+    private float time;
+
+
+    private void Awake() {
+        Instance = this;
+    }
 
     private void Start() {
         Lander.Instance.OnCoinPickup += Lander_OnCoinPickup;
         Lander.Instance.OnLanded += Lander_OnLanded;
+    }
+
+    private void Update() {
+        time += Time.deltaTime;
     }
 
     private void Lander_OnLanded(object sender, Lander.OnLandedEventArgs e) {
@@ -25,5 +36,12 @@ public class GameManager : MonoBehaviour {
         Debug.Log(score);
     }
 
+    public int GetScore() {
+        return score;
+    }
+
+    public float GetTime() {
+        return time;
+    }
 
 }
