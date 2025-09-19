100f39d8289eb66f130452e334b0cef51631522b
AliasAhshan <ahshan.ahmed003@gmail.com>
2025-09-18 20:37:46 -0400

    Setup inputactions for handling wasd and arrow keys

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -63,7 +63,7 @@ public class Lander : MonoBehaviour {
         switch (state) {
             default:
             case State.WaitingToStart:
-                if (Keyboard.current.upArrowKey.isPressed || Keyboard.current.leftArrowKey.isPressed || Keyboard.current.rightArrowKey.isPressed) {
+                if (GameInput.Instance.IsUpActionPressed() || GameInput.Instance.IsRightActionPressed() || GameInput.Instance.IsLeftActionPressed()) {
                     landerRigidbody2D.gravityScale = GRAVITY_NORMAL;
                     SetState(State.Normal);
                 }
@@ -74,21 +74,21 @@ public class Lander : MonoBehaviour {
                     return;
                 }
 
-                if (Keyboard.current.upArrowKey.isPressed || Keyboard.current.leftArrowKey.isPressed || Keyboard.current.rightArrowKey.isPressed) {
+                if (GameInput.Instance.IsUpActionPressed() || GameInput.Instance.IsRightActionPressed() || GameInput.Instance.IsLeftActionPressed()) {
                     ConsumeFuel();
                 }
 
-                if (Keyboard.current.upArrowKey.isPressed) {
+                if (GameInput.Instance.IsUpActionPressed()) {
                     float force = 700f;
                     landerRigidbody2D.AddForce(force * Time.deltaTime * transform.up);
                     OnUpForce?.Invoke(this, EventArgs.Empty);
                 }
-                if (Keyboard.current.leftArrowKey.isPressed) {
+                if (GameInput.Instance.IsLeftActionPressed()) {
                     float turnSpeed = +100f;
                     landerRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
                     OnLeftForce?.Invoke(this, EventArgs.Empty);
                 }
-                if (Keyboard.current.rightArrowKey.isPressed) {
+                if (GameInput.Instance.IsRightActionPressed()) {
                     float turnSpeed = -100f;
                     landerRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
                     OnRightForce?.Invoke(this, EventArgs.Empty);
